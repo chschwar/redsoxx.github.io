3019e30de20609acf1ef2c8ac7aa1e020f923c60
soxx <soxx@cryptolab.net>
2015-09-11 21:10:57 +0200

    - add assignment 1

--- Assignment.5/Assignment.5.js
+++ Assignment.5/Assignment.5.js
@@ -665,6 +665,10 @@ function Sphere(latitudeBands, longitudeBands, typeOfMapping)
                 this.numIndices+=6;
             }
         }
+        
+        
+        console.log(this.numVertices);
+        console.log(this.numIndices);
     }
     
     this.initSphereBuffers = function()
